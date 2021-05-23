a2efced6f05c0dd3e54d75e6a1e32cd200a7548a
yosuke1227 <63897708+yosuke1227@users.noreply.github.com>
2021-05-23 15:43:29 +0900

    Initial Commit

---
+++ GarbageCalender/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GarbageCalender
+//
+//  Created by Yosuke Yoshida on 2021/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GarbageCalender/GarbageCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarbageCalenderApp.swift
+//  GarbageCalender
+//
+//  Created by Yosuke Yoshida on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GarbageCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
